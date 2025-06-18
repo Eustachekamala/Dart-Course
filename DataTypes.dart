8039147125ff765c44d5c1af46ec0640d4b445d9
eustachekamala <eustachejared25@gmail.com>
2025-06-18 13:14:04 +0300

    Data Types

---
+++ DataTypes.dart
@@ -0,0 +1,23 @@
+void main(){
+  //Data Types
+  //Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+  //Strings
+  String name = "John Doe";
+  print("String: $name");
+  //Integers
+  int age = 45;
+  print("Integer: $age");
+
+  //Doubles
+  double percentage = 69.0;
+  print("Double: $percentage");
+
+  //Booleans
+  bool isOpen = true;
+  print("Boolean: $isOpen");
+
+  //Dynamic
+  dynamic fullname = "John Elder";
+  print("Dynamic: $fullname");
+}
