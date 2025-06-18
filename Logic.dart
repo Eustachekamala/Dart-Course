ddc61c9d9ab1b1297d6cadcae987dea9394555a3
eustachekamala <eustachejared25@gmail.com>
2025-06-18 14:07:34 +0300

    Logic with the if statements

---
+++ Logic.dart
@@ -0,0 +1,11 @@
+void main(){
+  //Logic -If, If/Else, If Else If
+  var age = 20;
+  if(age > 18){
+    print("You're allowed to vote");
+  } else if(age == 18){
+    print("You can go to vote");
+  }else {
+    print("You're still a teenager");
+  }
+}
