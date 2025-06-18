b93b2efe6c8c26ab620ca21caa47b709a9c69ba5
eustachekamala <eustachejared25@gmail.com>
2025-06-18 13:07:07 +0300

    variables

---
+++ Variables.dart
@@ -0,0 +1,19 @@
+void main() {
+  //Variables
+
+  //Strings
+  String name = "John";
+
+  //Integer
+  int age = 41;
+
+  //Dynamic
+  dynamic firstname = "Mark";
+
+  //Const and final
+  String fullname = name + firstname;
+  final String nickname = "Jared";
+
+  print('My name is $fullname and I am $age years old. My nickname is $nickname');
+
+}
