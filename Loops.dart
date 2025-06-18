a9485c3712e77eea6f556c3a778f429930a794d1
eustachekamala <eustachejared25@gmail.com>
2025-06-18 13:56:18 +0300

    Loops

---
+++ Loops.dart
@@ -0,0 +1,19 @@
+void main(){
+  //For Loop
+  for(int i = 0; i < 10; i++){
+    print(i);
+  }
+
+  //For In Loop
+  var students = ["Eustache", "Mary", "John", "Jared", "Rosette"];
+  for(var student in students){
+    print(student);
+  }
+
+  //While Loop
+  var num = 10;
+  while(num >=1){
+    print(num);
+    num--;
+  }
+}
