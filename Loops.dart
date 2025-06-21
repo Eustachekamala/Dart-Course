a287e6a502f990471fd4330f1459f873a80e3620
eustachekamala <eustachejared25@gmail.com>
2025-06-21 14:28:25 +0300

    added a list object that can be filtered by the element greater than 50

--- Loops.dart
+++ Loops.dart
@@ -1,19 +1,24 @@
 void main(){
   //For Loop
   for(int i = 0; i < 10; i++){
-    print(i);
+    // print(i);
   }
 
   //For In Loop
   var students = ["Eustache", "Mary", "John", "Jared", "Rosette"];
   for(var student in students){
-    print(student);
+    // print(student);
   }
 
   //While Loop
   var num = 10;
   while(num >=1){
-    print(num);
+    // print(num);
     num--;
+  }
+
+  List<int> scores = [20, 50, 75, 30, 80, 100];
+  for(int score in scores.where((s) => s > 50)){
+      print("The score is $score");
   }
 }
