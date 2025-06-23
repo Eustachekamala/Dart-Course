4879d08a48c5a21a6363db5f7e556e5ba500c4e3
eustachekamala <eustachejared25@gmail.com>
2025-06-23 13:26:28 +0300

    work with the http protocols

--- Futures.dart
+++ Futures.dart
@@ -1,16 +1,24 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
 void main() async{
   //Futures --> like promises in Js
   //Can have uncompleted or completed state
   final post = await fetchPost();
-  print(post.userId);
   print(post.title);
+  print(post.userId);
 }
 
-Future<Post> fetchPost(){
-  const delay = Duration(seconds: 3);
-  return Future.delayed(delay, (){
-    return Post('My Post', 12);
-  });
+Future<Post> fetchPost() async{
+  var url =
+      Uri.https('jsonplaceholder.typicode.com', '/posts/1');
+  final res = await http.get(url);
+      if(res.statusCode == 200){
+    Map<String, dynamic> data = convert.jsonDecode(res.body);
+    return Post(data["title"], data["userId"]);
+  } else {
+    throw Exception("Failed to load post: ${res.statusCode}");
+  }
 }
 
 class Post {
